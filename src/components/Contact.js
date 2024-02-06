7a9377c6a52493073da5a1378d82abb35f603d41
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-02-06 17:56:59 -0500

    eneabled netlify forms

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -78,6 +78,7 @@ export const Contact = () => {
               data-netlify="true"
               name="contactForm"
               action="/thank-you"
+              netlify
             >
               <input type="hidden" name="form-name" value="contactForm" />
 
